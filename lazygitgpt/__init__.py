151ff5f65d7ae977437411be0482a70be73483b1
Shankar Pandala <shankar.pandala@gmail.com>
2023-11-19 22:59:10 +0530

    Bump version: 0.0.5 → 0.0.6-beta

--- lazygitgpt/__init__.py
+++ lazygitgpt/__init__.py
@@ -3,6 +3,6 @@ lazygitgpt: A Python package to manage git repositories using GPT-4.
 """
 
 # Package-level dunder variables
-__version__ = "0.0.5"
+__version__ = "0.0.6-beta"
 __author__ = "Shankar Pandala"
 __email__ = "shankar@ssbm.ch"
