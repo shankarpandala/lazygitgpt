28237818fbd57eaa0a74c54051e84fb4ddd31cb7
Shankar Pandala <shankar.pandala@gmail.com>
2023-11-19 22:00:16 +0530

    added main.py

--- lazygitgpt/__init__.py
+++ lazygitgpt/__init__.py
@@ -6,6 +6,3 @@ lazygitgpt: A Python package to manage git repositories using GPT-4.
 __version__ = "0.0.5-beta"
 __author__ = "Shankar Pandala"
 __email__ = "shankar@ssbm.ch"
-
-# Importing the main functionalities to be accessible at package level
-from .cli import main
---
+++ lazygitgpt/__main.py__.py
@@ -0,0 +1,3 @@
+from lazygitgpt.cli import main
+
+main()
