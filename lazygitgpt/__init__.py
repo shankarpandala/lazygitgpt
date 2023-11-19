886d92d75a9008b0e503d24e2c4057180f716386
Shankar Pandala <shankar.pandala@gmail.com>
2023-11-19 23:04:21 +0530

    Add import statement for CLI module

--- lazygitgpt/__init__.py
+++ lazygitgpt/__init__.py
@@ -6,3 +6,5 @@ lazygitgpt: A Python package to manage git repositories using GPT-4.
 __version__ = "0.0.6-beta"
 __author__ = "Shankar Pandala"
 __email__ = "shankar@ssbm.ch"
+
+from .cli import main
