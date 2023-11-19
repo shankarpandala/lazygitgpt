af487cbda5b7cb0113afb3b1ea468c3f8f62f120
Shankar Pandala <shankar.pandala@gmail.com>
2023-11-19 15:46:27 +0530

    Bump version to 0.0.4

--- lazygitgpt/__init__.py
+++ lazygitgpt/__init__.py
@@ -3,7 +3,7 @@ lazygitgpt: A Python package to manage git repositories using GPT-4.
 """
 
 # Package-level dunder variables
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __author__ = "Shankar Pandala"
 __email__ = "shankar@ssbm.ch"
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Define the package version
-version = "0.0.3"
+version = "0.0.4"
 
 # Define the long description from the README file
 with open("README.md", "r", encoding="utf-8") as fh:
