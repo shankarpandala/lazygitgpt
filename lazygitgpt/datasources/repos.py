1fde4d0c373e8c1ec94978f6a72a52604cca9320
Shankar Pandala <shankar.pandala@gmail.com>
2023-11-26 22:44:38 +0530

    updated to use langchain gitloader

--- lazygitgpt/datasources/repos.py
+++ lazygitgpt/datasources/repos.py
@@ -1,26 +1,30 @@
 import os
 import glob
 import json
+from git import Repo
+from langchain.document_loaders import GitLoader
+from langchain.document_loaders.generic import GenericLoader
+from langchain.document_loaders.parsers import LanguageParser
+from langchain.text_splitter import Language
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
-def read_repository_contents(directory_path=os.getcwd(), file_pattern="*"):
-    """
-    Reads all files in the specified directory matching the file pattern,
-    and creates a JSON object with file names and their contents.
+def read_repository_contents():
+    repo_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    Repo.git_dir=repo_path
+    repo = Repo(Repo.git_dir)
+    branch = repo.head.reference
 
-    Args:
-    directory_path (str): Path to the directory containing the files.
-    file_pattern (str): Pattern to match files. Defaults to '*' (all files).
-
-    Returns:
-    str: A JSON string containing the file names and their contents.
-    """
-    data = {}
-    for file_path in glob.glob(f"{directory_path}/{file_pattern}"):
-        if os.path.isfile(file_path):
-            try:
-                with open(file_path, 'r', encoding='utf-8') as file:
-                    data[file_path] = file.read()
-            except Exception as e:
-                print(f"Error reading file: {file_path} - {e}")
-    
-    return json.dumps(data, indent=4)
+    loader = GitLoader(repo_path, branch=branch)
+    docs = loader.load()
+    loader = GenericLoader.from_filesystem(
+        repo_path,
+        glob="**/*",
+        suffixes=[".py"],
+        parser=LanguageParser(language=Language.PYTHON, parser_threshold=500),
+    )
+    documents = loader.load()
+    python_splitter = RecursiveCharacterTextSplitter.from_language(
+        language=Language.PYTHON, chunk_size=2000, chunk_overlap=200
+    )
+    texts = python_splitter.split_documents(documents)
+    return texts
